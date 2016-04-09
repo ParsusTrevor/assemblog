12bab520a5dc2fd837da838e23e8ec274b9abe5b
Trevor Abell <trevor@keikocode.com>
2016-04-09 09:46:09 -0700

    re #1 re #2 re #3 - refactored to get all tickets from milestone, uses env node for cli

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 var request = require('request');
 var EOL = require('os').EOL;
 var fs      = require('fs');
 var _       = require('lodash');
-
-
+var _debug = console.log;              
+              
 var API_KEY = process.env.API_KEY;
 var API_SECRET = process.env.API_SECRET;
 
@@ -12,67 +14,71 @@ var changelog = process.argv[3];
 
 var urlbase = 'https://api.assembla.com/v1/spaces/acdhh/';
 var options = {
-    url: urlbase + 'milestones',
-    headers: {
-        'X-Api-Key': API_KEY,
-        'X-Api-Secret': API_SECRET
-    }
+  url: urlbase + 'milestones',
+  headers: {
+    'X-Api-Key': API_KEY,
+    'X-Api-Secret': API_SECRET
+  }
 };
-
+            
 var milestoneId = '';
 request(options, function (error, response, body) {
+  if(!error && response.statusCode == 200) {
+    var data = JSON.parse(body);
+        
+    milestoneId = _.filter(data, function (item) {
+      return item.title === milestone;
+    })[0].id;
+  } else {
+    _debug(response);
+  }
+          
+  options.url = urlbase + 'tickets' + '/' + 'milestone' + '/' + milestoneId + '?per_page=100';
+
+  options.headers.per_page = 100;
+  
+  var lines = [];
+  
+  request(options, function (error, response, body) {
     if(!error && response.statusCode == 200) {
-        var data = JSON.parse(body);
-
-        milestoneId = _.filter(data, function (item) {
-            return item.name === milestone;
-        })[0];
-    } else {
-        console.log(response);
-    }
-});
-
-options.url = urlbase + 'tickets';
-
-var lines = [];
-
-request(options, function (error, response, body) {
-    if(!error && response.statusCode == 200) {
-        var data = JSON.parse(body);
-
-        var milestoneTickets = _.filter(data, function (item) {
-            return item.milestone_id === milestoneId;
-        });
-
-        var header = '## ' + milestone;
+      var data = JSON.parse(body);
+      
+      // var milestoneTickets = _.filter(data, function (item) {
+      //     return item.milestone_id === milestoneId;
+      // });
+      
+      var milestoneTickets = data;
+          
+      var header = '## ' + milestone;
         lines.push(header);
-
+        
         lines.push('### Added');
         _.map(data, function (ticket) {
-            var entry = '- ' + ticket.summary + ' ([#' + ticket.number + '](' + 'http://www.assembla.com/spaces/acdhh/tickets/' + ticket.number + '))';
-
-            lines.push(entry);
+          var entry = '- ' + ticket.summary + ' ([#' + ticket.number + '](' + 'http://www.assembla.com/spaces/acdhh/tickets/' + ticket.number + '))';
+          
+          lines.push(entry);
         });
-
-        lines.push('### Changed');
-        lines.push('### Fixed');
-
-        fs.readFile(changelog, function(err, data) {
-            if (err) throw err;
-            
-            var filelines = _.split(data, EOL);
-
-            var args = [4, 0].concat(lines);
-            Array.prototype.splice.apply(filelines, args);
-            var buffer = new Buffer(_.join(filelines, EOL), 'utf8');
-            
-            fs.open(changelog, 'w+', function(err, fd) {
-                if(err) throw err;
-                
-                fs.write(fd, buffer, 0, buffer.length, 0);
-            });
+      
+      lines.push('### Changed');
+      lines.push('### Fixed');
+      
+      fs.readFile(changelog, function(err, data) {
+        if (err) throw err;
+        
+        var filelines = _.split(data, EOL);
+        
+        var args = [4, 0].concat(lines);
+        Array.prototype.splice.apply(filelines, args);
+        var buffer = new Buffer(_.join(filelines, EOL), 'utf8');
+        
+        fs.open(changelog, 'w+', function(err, fd) {
+          if(err) throw err;
+          
+          fs.write(fd, buffer, 0, buffer.length, 0);
         });
+      });
     } else {
-        console.log(response);
+      _debug(response);
     }
+  });        
 });
