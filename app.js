5220850bd188b10a9d3b6f1af5a4690077ab3727
Trevor Abell <trevor@keikocode.com>
2016-04-10 09:26:02 -0700

    re #5 Added request promise and refactored to use promises

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-var request = require('request');
+var request = require('request-promise');
 var EOL = require('os').EOL;
 var fs      = require('fs');
 var _       = require('lodash');
-var _debug = console.log;              
-              
+
 var API_KEY = process.env.API_KEY;
 var API_SECRET = process.env.API_SECRET;
 
+var _debug = console.log;
 var milestone = process.argv[2];
 var changelog = process.argv[3];
 
@@ -20,65 +20,60 @@ var options = {
     'X-Api-Secret': API_SECRET
   }
 };
-            
-var milestoneId = '';
-request(options, function (error, response, body) {
-  if(!error && response.statusCode == 200) {
-    var data = JSON.parse(body);
-        
-    milestoneId = _.filter(data, function (item) {
-      return item.title === milestone;
-    })[0].id;
-  } else {
-    _debug(response);
-  }
-          
-  options.url = urlbase + 'tickets' + '/' + 'milestone' + '/' + milestoneId + '?per_page=100';
 
-  options.headers.per_page = 100;
-  
+
+function findMatchingMilestoneId(body) {
+  var milestoneId;
+
+  var data = JSON.parse(body);
+  milestoneId = _.filter(data, function (item) {
+    return item.title === milestone;
+  })[0].id;
+
+  return milestoneId;
+};
+
+function requestTickets(milestoneId) {
+    options.url = urlbase + 'tickets' + '/' + 'milestone' + '/' + milestoneId + '?per_page=100';
+    return request(options);
+}
+
+function writeChangeLog(body) {
   var lines = [];
-  
-  request(options, function (error, response, body) {
-    if(!error && response.statusCode == 200) {
-      var data = JSON.parse(body);
-      
-      // var milestoneTickets = _.filter(data, function (item) {
-      //     return item.milestone_id === milestoneId;
-      // });
-      
-      var milestoneTickets = data;
-          
-      var header = '## ' + milestone;
-        lines.push(header);
-        
-        lines.push('### Added');
-        _.map(data, function (ticket) {
-          var entry = '- ' + ticket.summary + ' ([#' + ticket.number + '](' + 'http://www.assembla.com/spaces/acdhh/tickets/' + ticket.number + '))';
-          
-          lines.push(entry);
-        });
-      
-      lines.push('### Changed');
-      lines.push('### Fixed');
-      
-      fs.readFile(changelog, function(err, data) {
-        if (err) throw err;
-        
-        var filelines = _.split(data, EOL);
-        
-        var args = [4, 0].concat(lines);
-        Array.prototype.splice.apply(filelines, args);
-        var buffer = new Buffer(_.join(filelines, EOL), 'utf8');
-        
-        fs.open(changelog, 'w+', function(err, fd) {
-          if(err) throw err;
-          
-          fs.write(fd, buffer, 0, buffer.length, 0);
-        });
+  var data = JSON.parse(body);
+  var milestoneTickets = data;
+
+  var header = '## ' + milestone;
+    lines.push(header);
+
+    lines.push('### Added');
+    _.map(data, function (ticket) {
+      var entry = '- ' + ticket.summary + ' ([#' + ticket.number + '](' + 'http://www.assembla.com/spaces/acdhh/tickets/' + ticket.number + '))';
+
+      lines.push(entry);
+    });
+
+    lines.push('### Changed');
+    lines.push('### Fixed');
+
+    fs.readFile(changelog, function(err, data) {
+      if (err) throw err;
+
+      var filelines = _.split(data, EOL);
+      var args = [4, 0].concat(lines);
+      Array.prototype.splice.apply(filelines, args);
+      var buffer = new Buffer(_.join(filelines, EOL), 'utf8');
+      fs.open(changelog, 'w+', function(err, fd) {
+        if(err) throw err;
+        fs.write(fd, buffer, 0, buffer.length, 0);
       });
-    } else {
-      _debug(response);
-    }
-  });        
-});
+    });
+};
+
+request(options)
+    .then(findMatchingMilestoneId)
+    .then(requestTickets)
+    .then(writeChangeLog)
+    .catch(function (err) {
+        _debug(err);
+    });
